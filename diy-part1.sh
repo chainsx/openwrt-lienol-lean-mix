1fd3cf69b5fde7512e3262c4ea7df1256ebaba3c
chainsx(cx-dandelion) <pzhrhlx@outlook.com>
2020-05-25 15:50:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,3 +13,10 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+#get lean package
+wget https://github.com/coolsnowwolf/lede/archive/20200408.tar.gz
+tar -zxvf 20200408.tar.gz
+cp -rfp lede-20200408/package/lean package
+git clone https://github.com/chongteng2017/luci-app-passwall package/lean/luci-app-passwall
+rm -rf 20200408*
