fb3a91d41d771efa1fad37dfadda5914fbf2eeb6
chainsx(cx-dandelion) <pzhrhlx@outlook.com>
2020-12-10 01:48:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,14 +13,3 @@
 
 # Add a feed source
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-
-cat <<EOF >> patch.sh
-wget https://github.com/melsem/openwrt-lede_xradio-xr819_soc-audio/raw/master/packages_xradio-OpenWrt-master/add-patch_dts_file-wifi-xradio.patch
-patch -p1 < add-patch_dts_file-wifi-xradio.patch | y
-
-wget https://github.com/melsem/openwrt-lede_xradio-xr819_soc-audio/raw/master/add-SOC_AUDIO-usb2-usb3-opi_zero_openwrt-master-4.19/add-SOC_AUDIO-usb2-usb3-opi_zero_openwrt-master.patch
-patch -p1 < add-SOC_AUDIO-usb2-usb3-opi_zero_openwrt-master.patch
-
-https://github.com/melsem/openwrt-lede_xradio-xr819_soc-audio/raw/master/add-SOC_AUDIO-usb2-usb3-opi_zero_openwrt-master-4.19/add-on_wifi-mac80211.patch
-patch -p1 < add-on_wifi-mac80211.patch
-EOF
